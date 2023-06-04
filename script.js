c5d8d4f958622f956ffec682adf90699a6187cf2
Rahul Thakur <133585625+rahulthakur1995@users.noreply.github.com>
2023-06-04 16:53:04 +0530

    add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 50);
+});
+
+
+
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-bars");
+  });
+});
+
